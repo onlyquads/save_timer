54d91c4625149a3e1e400bf1a74759a37f32f80d
Nicolas Garcia <garciatunonnicolas@gmail.com>
2024-02-03 22:00:28 +0100

    Fix shelve cleanup logical

--- save_timer/save_timer.py
+++ save_timer/save_timer.py
@@ -14,7 +14,6 @@ mc.evalDeferred(
 # COMPATIBILITY:
 Maya 2022 and above
 
-# TODO: -Add ShelfTab changed behavior: remove existing button + create button
 '''
 
 import maya.cmds as mc
@@ -35,15 +34,27 @@ TIMER_STATES = [
     (480.0, 'SAVE NOW!', (1.0, 0.0, 0.0))
     ]
 
+# SAVE_CMD = 'import maya.cmds as mc; mc.file(type="mayaAscii", save=True)'
+SAVE_CMD = ''
+
+SHELF_CALLBACK_NAMES = {
+    'toggleShelfTabs',
+    'shelfTabRefresh',
+    'shelfTabChange'
+}
+
 
 class SaveTimer(QWidget):
     def __init__(self):
+        self.shelf_timer_button = None
+        self.top_shelf = None
+        self.current_shelf = None
 
         self.timer = QTimer()
-        self.timer.timeout.connect(self.check_timer)
+        self.timer.timeout.connect(self.update_button)
         self.elapsed_timer = QElapsedTimer()
 
-        self.remove_from_shelf()
+        self.shelves_cleanup()
         self.create_button()
 
         # Register callbacks
@@ -56,41 +67,56 @@ class SaveTimer(QWidget):
         om.MSceneMessage.addCallback(
             om.MSceneMessage.kAfterNew, self.on_scene_new)
 
-        print('Save Timer Loaded')
+        om.MSceneMessage.addCallback(
+            om.MSceneMessage.kMayaExiting, self.on_maya_exit)
+
+        # This registers om callbacks mostly for user shelf interaction
+        self.om_callbacks = om.MCommandMessage.addProcCallback(
+            self.cmd_after_callback, None)
+
+        print('Save Timer script has started')
 
     def on_scene_save(self, *args):
         # print('scene saved')
         if not self.timer.isActive():
             self.timer.start(TIMER_INTERVAL)
         self.elapsed_timer.start()
-        self.check_timer()
+        self.update_button()
 
     def on_scene_open(self, *args):
         # print('scene opened')
         if not self.timer.isActive():
             self.timer.start(TIMER_INTERVAL)
         self.elapsed_timer.start()
-        self.check_timer()
+        self.update_button()
 
     def on_scene_new(self, *args):
         # print('New scene')
         if self.timer.isActive():
             self.timer.stop()
-        self.update_button(TIMER_STATES[0][1], TIMER_STATES[0][2])
+        self.update_button()
 
-    def check_timer(self):
+    def on_maya_exit(self, *args):
+        self.shelves_cleanup()
+
+    def get_current_state(self):
         # print('check timer')
         elapsed_time = float(self.elapsed_timer.elapsed() // 1000)
+        if elapsed_time < 0:
+            elapsed_time = -1.0
         # Check elapsed time against thresholds
         for threshold, text_label, bg_color in TIMER_STATES:
             if elapsed_time <= threshold:
-                self.update_button(text_label, bg_color)
-                return
-        # print('stop timer')
+                # self.update_button()
+                return text_label, bg_color
+        # Stop our timer because we reach end of states
         self.timer.stop()
 
-    def update_button(self, text_label, bg_color):
+    def update_button(self):
         # print('Button updated')
+        if not self.shelf_timer_button:
+            return
+        text_label, bg_color = self.get_current_state()
         mc.shelfButton(
             self.shelf_timer_button,
             e=True,
@@ -100,22 +126,21 @@ class SaveTimer(QWidget):
 
     def create_button(self):
         # print('Button Created')
-        command = ''
         shelf_top_level = mm.eval('$temp = $gShelfTopLevel')
         current_shelf = mc.tabLayout(
             shelf_top_level, query=True, selectTab=True)
         mc.setParent(current_shelf)
+        label_text, bg_color = self.get_current_state()
         self.shelf_timer_button = mc.shelfButton(
-            label=TIMER_STATES[0][1],
-            useTemplate=TIMER_STATES[0][1],
+            label=label_text,
             annotation='save timer click to save',
             style='textOnly',
             font='plainLabelFont',
             align='center',
             width=120,
-            backgroundColor=(1, 0, 0),
+            backgroundColor=bg_color,
             preventOverride=True,
-            command=command)
+            command=SAVE_CMD)
         mc.shelfButton(self.shelf_timer_button, edit=True, width=120)
         mc.shelfLayout(current_shelf, e=True, pos=(self.shelf_timer_button, 1))
 
@@ -127,20 +152,43 @@ class SaveTimer(QWidget):
             f'{shelf_top_level}|{current_shelf}|{TOOLNAME}')
         return button_path
 
-    def remove_from_shelf(self):
-        top_shelf = mm.eval(
-            'global string $top_shelf; $temp = $gShelfTopLevel;')
-        currentShelf = mc.tabLayout(top_shelf, q=True, st=True)
-        buttons = mc.shelfLayout(currentShelf, q=True, ca=True)
-        # Combine all the possible save timer button names in a list
+    def shelves_cleanup(self):
 
+        if not self.current_shelf:
+            self.top_shelf = mm.eval('$temp = $gShelfTopLevel;')
+            self.current_shelf = mc.tabLayout(self.top_shelf, q=True, st=True)
+
+        buttons = mc.shelfLayout(self.current_shelf, q=True, ca=True)
+
+        if not buttons:
+            return
         for button in buttons:
-            button_name = mc.shelfButton(button, q=True, label=True)
-            if any(button_name == name[1] for name in TIMER_STATES):
+            if mc.objectTypeUI(button) != 'shelfButton':
+                continue
+            button_label = mc.shelfButton(button, q=True, label=True)
+            if any(button_label == label[1] for label in TIMER_STATES):
                 # last check if the shelf button exists before deleting it
                 if mc.shelfButton(button, exists=True):
+                    # print(f'Deleting button named : {button_label}')
                     mc.deleteUI(button)
 
-    def on_shelf_tab_change(self):
-        # print('shelf tab changed!')
+        self.top_shelf = mm.eval('$temp = $gShelfTopLevel;')
+        self.current_shelf = mc.tabLayout(self.top_shelf, q=True, st=True)
+
+    def cmd_after_callback(
+            self, proc_callback, invocation_id, bool, value, user_data):
+        # Filter only positive callbacks
+        if value != om.MCommandMessage.kMELProc or not bool:
+            return
+        # Filter only wanted callbacks
+        if proc_callback in SHELF_CALLBACK_NAMES:
+            self.shelf_tab_changed()
+
+    def remove_callbacks(self):
+        om.MMessage.removeCallback(self.om_callbacks)
+
+    def shelf_tab_changed(self):
+        mc.evalDeferred(self.shelves_cleanup, lowestPriority=True)
+        mc.evalDeferred(self.create_button, lowestPriority=True)
+        self.update_button()
         return
